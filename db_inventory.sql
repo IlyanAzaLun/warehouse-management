9199c3e32b7b42b7e146fd82e9fbe49e0fd04576
ilyanazalun <iyang_agung_s@hotmail.com>
2021-11-15 10:16:22 +0700

    update

--- db_inventory.sql
+++ db_inventory.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Nov 10, 2021 at 04:09 AM
+-- Generation Time: Nov 15, 2021 at 04:15 AM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 7.3.27
 
@@ -20,6 +20,54 @@ SET time_zone = "+00:00";
 --
 -- Database: `db_inventory`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_customer`
+--
+
+CREATE TABLE `tbl_customer` (
+  `customer_id` varchar(255) NOT NULL,
+  `customer_fullname` varchar(255) NOT NULL,
+  `customer_address` text NOT NULL,
+  `customer_contact_phone` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_customer`
+--
+
+INSERT INTO `tbl_customer` (`customer_id`, `customer_fullname`, `customer_address`, `customer_contact_phone`) VALUES
+('416e5e79-4539-11ec-8158-5cac4cba0f32', 'Dimas Hermawan', '-', '-'),
+('7388053e-4506-11ec-8158-5cac4cba0f32', 'Iyang Agung Supriatna', 'Jl. Tanjungkerta, Dsn. Panteneun Rt 004/Rw 006, Ds Licin, Kec. Cimalaka, Kab. Sumedang', '628986102327');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_invoice`
+--
+
+CREATE TABLE `tbl_invoice` (
+  `invoice_id` varchar(255) NOT NULL,
+  `date` varchar(255) NOT NULL,
+  `date_due` varchar(255) NOT NULL,
+  `to_customer_destination` varchar(255) NOT NULL,
+  `order_id` varchar(255) NOT NULL,
+  `sub_total` varchar(255) NOT NULL,
+  `tax` varchar(255) NOT NULL,
+  `discount` varchar(3) NOT NULL,
+  `grant_total` varchar(255) NOT NULL,
+  `status_payment` tinyint(1) NOT NULL,
+  `note` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_invoice`
+--
+
+INSERT INTO `tbl_invoice` (`invoice_id`, `date`, `date_due`, `to_customer_destination`, `order_id`, `sub_total`, `tax`, `discount`, `grant_total`, `status_payment`, `note`) VALUES
+('8e3a2326-4509-11ec-8158-5cac4cba0f32', '1636431432', '1637465803', '7388053e-4506-11ec-8158-5cac4cba0f32', 'f51143c5-4508-11ec-8158-5cac4cba0f32', '', '', '', '', 1, '');
 
 -- --------------------------------------------------------
 
@@ -43,10 +91,29 @@ CREATE TABLE `tbl_item` (
 --
 
 INSERT INTO `tbl_item` (`item_code`, `item_name`, `item_category`, `quantity`, `unit`, `capital_price`, `selling_price`, `image_id`) VALUES
-('ACC-1', 'Battery lithium', 'Accessories', 250, 'pcs', '20000', '30000', 'assets/image/LQD-001.jpg'),
-('LQD-ML-12', 'MARJAN ACB', 'Liquid', 12, 'pac', '85000', '105000', 'assets/image/LQD-001.jpg'),
-('LQD-ML001', 'Acai Pomegranate Nic Salt E-Liquid by Bloom', 'Liquid', 1000, 'pcs', '80000', '100000', 'assets/image/LQD-001.jpg'),
-('LQD-NIKOTIN-1', 'MARJAN', 'Liquid', 12, 'pac', '80000', '100000', 'assets/image/LQD-001.jpg');
+('ACC-0001', 'Charger Battery lithium', 'ACC', 2500, 'pac', '20000', '30000', 'assets/images/LQD-001.jpg'),
+('LIQUID-FREEBASE-CREAMY-0001', 'Acai Pomegranate Nic Salt E-Liquid by Bloom', 'LIQUID', 250, 'pac', '150000', '200000', 'assets/images/LQD-001.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_order`
+--
+
+CREATE TABLE `tbl_order` (
+  `order_id` varchar(255) NOT NULL,
+  `item_id` varchar(255) NOT NULL,
+  `quantity` int(255) NOT NULL,
+  `unit` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_order`
+--
+
+INSERT INTO `tbl_order` (`order_id`, `item_id`, `quantity`, `unit`) VALUES
+('f51143c5-4508-11ec-8158-5cac4cba0f32', 'LIQUID-FREEBASE-CREAMY-0001', 20, 'pac'),
+('f51143c5-4508-11ec-8158-5cac4cba0f32', 'LIQUID-ACC-0001', 10, 'pac');
 
 -- --------------------------------------------------------
 
@@ -141,6 +208,7 @@ CREATE TABLE `tbl_user_menu` (
 INSERT INTO `tbl_user_menu` (`menu_id`, `parent_id`, `category_id`, `title`, `url`, `menu_controller`, `icon`, `is_active`) VALUES
 ('14f5eed6-0fac-4375-a7d3-bb51f28d3c86', 'a93e3526-4134-11ec-ae08-0d3b0460d819', 'dec9f1e2-4127-11ec-ae08-0d3b0460d819', 'Role', 'configuration/role', 'Role', 'fa fa-tw fa-dice', 1),
 ('2b008579-4135-11ec-ae08-0d3b0460d819', 'a93e3526-4134-11ec-ae08-0d3b0460d819', 'dec9f1e2-4127-11ec-ae08-0d3b0460d819', 'Menu', 'configuration/menu', 'Menu', 'fas fa-tw fa-th-large', 1),
+('34bb5023-c344-4a17-afcf-b5a986e7911c', '', '85f50b77-69ec-44e7-8a54-def0e8a1efec', 'Invoice item', 'invoice', 'invoice', 'fa fa-tw fa-file-invoice', 1),
 ('52eacc07-eed3-43e8-9e93-0310749aa3be', '', '85f50b77-69ec-44e7-8a54-def0e8a1efec', 'List item', 'items', 'Items', 'fa fa-tw fa-boxes', 1),
 ('a87acd37-4166-11ec-ae08-0d3b0460d819', '', '41baae76-4166-11ec-ae08-0d3b0460d819', 'Dashboard', 'dashboard', 'Welcome', 'fas fa-tachometer-alt', 1),
 ('a93e3526-4134-11ec-ae08-0d3b0460d819', '', 'dec9f1e2-4127-11ec-ae08-0d3b0460d819', 'Configuration', '', '', 'fa fa-tw fa-cog', 1),
@@ -170,6 +238,18 @@ INSERT INTO `tbl_user_menu_category` (`category_id`, `category_name`) VALUES
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `tbl_customer`
+--
+ALTER TABLE `tbl_customer`
+  ADD PRIMARY KEY (`customer_id`);
+
+--
+-- Indexes for table `tbl_invoice`
+--
+ALTER TABLE `tbl_invoice`
+  ADD PRIMARY KEY (`invoice_id`);
 
 --
 -- Indexes for table `tbl_item`
